d9754e919b39ce1599a3a59d171899b324aa8120
7o8v <269134615@qq.com>
2022-09-02 02:24:22 +0000

    add version in package and fix version in setup.py

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -9,5 +9,7 @@ from .process import *
 
 # 检查版本
 
+version = "1.2"
+
 if sys.version_info.major != 3:
     raise PyUtilsException("PyUtils only support python3!")
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyutils', 
-    version='1.1', 
+    version='1.2', 
     author='7o8v',
     packages=['pyutils']
 )
