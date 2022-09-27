1f5ad526cee0b1025e8798cd751a86246506149d
7o8v <269134615@qq.com>
2022-09-27 16:00:36 +0800

    新增Event和EventTarget

--- pyutils/process.py
+++ pyutils/process.py
@@ -1,9 +1,14 @@
 import os
 import signal
 import subprocess
+import multiprocessing
+
+from queue import Empty
+from typing import Callable
+from threading import Thread
 
 __all__ = [
-    'kill_process', 'async_execute'
+    'kill_process', 'async_execute', 'Event', 'EventTarget'
 ]
 
 def kill_process(pid):
@@ -24,3 +29,62 @@ def async_execute(args:list, env:dict=None) -> subprocess.Popen:
         close_fds=False
     )
     return process
+
+class Event:
+
+    def __init__(self, name:str, **args):
+        self.__name = name
+        self.__args = args
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def args(self):
+        return self.__args
+
+    def get_arg(self, k):
+        return self.__args.get(k)
+
+class EventTarget:
+    '''
+        模仿javascript的EventTarget设计，可以用来实现IPC。
+    '''
+
+    def __init__(self):
+        self.__event_queue:multiprocessing.Queue[Event] = multiprocessing.Queue()
+        self.__handler_lock = multiprocessing.Lock()
+        self.__event_loop_running = True
+        self.__event_handlers = {}
+
+    def __event_loop(self):
+        while self.__event_loop_running:
+            try:
+                event = self.__event_queue.get(block=True, timeout=0.01)
+            except Empty:
+                continue
+            if not event: continue
+            with self.__handler_lock:
+                if event.name not in self.__event_handlers: continue
+                handler = self.__event_handlers[event.name]
+            handler(self, event)
+
+    def start_event_loop(self):
+        self.__event_loop_running = True
+        Thread(target=self.__event_loop, daemon=True).start()
+    
+    def stop_event_loop(self):
+        self.__event_loop_running = False
+    
+    def dispatch_event(self, event:Event):
+        self.__event_queue.put(event)
+    
+    def add_event_handler(self, event_name:str, handler:Callable):
+        with self.__handler_lock:
+            self.__event_handlers[event_name] = handler
+    
+    def remove_event_handler(self, event_name:str):
+        with self.__handler_lock:
+            if event_name in self.__event_handlers: return
+            self.__event_handlers.pop(event_name)
