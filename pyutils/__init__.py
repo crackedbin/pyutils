bb3de87cc75cc94b323c6bae928e2c747e3b3b35
7o8v <269134615@qq.com>
2022-11-02 09:20:55 +0800

    去掉版本检查

--- pyutils/__init__.py
+++ pyutils/__init__.py
@@ -7,9 +7,4 @@ from .logger import *
 from .misc import *
 from .process import *
 
-# 检查版本
-
 version = "1.5.2"
-
-if sys.version_info.major != 3:
-    raise PyUtilsException("PyUtils only support python3!")
